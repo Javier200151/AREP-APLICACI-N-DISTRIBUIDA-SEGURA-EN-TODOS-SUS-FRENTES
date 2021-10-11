66381f09140a2b90448ba29d4445b332732fc543
Javier López <javier200151@gmail.com>
2021-10-10 23:31:40 -0500

    llaves

--- src/main/java/edu/escuelaing/arep/HelloService.java
+++ src/main/java/edu/escuelaing/arep/HelloService.java
@@ -1,4 +1,4 @@
-package edu.escuelaing.arep;
+package cd ;
 
 import static spark.Spark.*;
 
@@ -11,7 +11,15 @@ public class HelloService
     public static void main( String[] args )
     {
         //API: secure(keystoreFilePath, keystorePassword, truststoreFilePath, truststorePassword); 
-        secure("keystores/ecikeystore.p12", "123456", null, null);
+        secure("keystores/ecikeystore.p12", "123456", "keystores/myTrustStore","123456");
+        port(getPort());
         get("/hello", (req, res) -> "Hello World");
     }
+    
+    static int getPort() { 
+        if (System.getenv("PORT") != null) { 
+            return Integer.parseInt(System.getenv("PORT")); 
+        } 
+        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost) 
+    } 
 }
